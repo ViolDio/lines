e9c91fac03a3fe8261a3592ddb147599750c42b2
ViolDio <dolinina170696@gmail.com>
2024-11-07 16:41:03 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string = input('Какое количество символов с слове?')
+print('Количество символов: ', my_string)
+
+
+print('my_string'.upper())
+print('my_string'.lower())
+print('my_string'.replace('my_string','mystring'))
+print('my_string'.replace('my_string','m'))
+print('my_string'.replace('my_string','g'))
